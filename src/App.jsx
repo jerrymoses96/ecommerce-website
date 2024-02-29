a8cf229f116674fa108ff86545ca9a60dadfca10
jerin <jerinkunnamkada@gmail.com>
2024-02-29 11:11:54 +0530

    redux persistor added

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,10 @@ import AppStore from "./utils/AppStore";
 import React, { useContext, useEffect, useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
+let persistor = persistStore(AppStore);
 export const userContext = React.createContext();
 
 const App = () => {
@@ -32,23 +35,25 @@ const App = () => {
   return (
     <userContext.Provider value={{ userdata, updateUserData }}>
       <Provider store={AppStore}>
-        <div className="overflow-x-hidden w-[100%] ">
-          <Header />
-          <Outlet />
-          <Footer />
-        </div>
-        <ToastContainer
-          position="top-center"
-          autoClose={1000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="dark"
-        />
+        <PersistGate persistor={persistor}>
+          <div className="overflow-x-hidden w-[100%] ">
+            <Header />
+            <Outlet />
+            <Footer />
+          </div>
+          <ToastContainer
+            position="top-center"
+            autoClose={1000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="dark"
+          />
+        </PersistGate>
       </Provider>
     </userContext.Provider>
   );
--- src/utils/AppStore.jsx
+++ src/utils/AppStore.jsx
@@ -1,11 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./CartSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const reducer = combineReducers({
+  cart: cartReducer, 
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 const AppStore = configureStore({
-    reducer:{
-        cart: cartReducer,
-    }
-    
+  reducer: persistedReducer
 });
 
 export default AppStore;
